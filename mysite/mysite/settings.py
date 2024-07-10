b3247fad4258b6595aeed4c901f53cc5809b44f3
Ruslan <ruslan.salmin@gmail.com>
2024-07-10 14:41:51 +0300

    Home_Work_django
    Signed-off-by: Ruslan <ruslan.salmin@gmail.com>

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,6 +25,8 @@ SECRET_KEY = 'django-insecure-7334&wb6$t&y=b&_hf(34m%_33w0=#&d_j#8cw0qa@au+tj_@4
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+
 
 ALLOWED_HOSTS = []
 
